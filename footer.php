5735d8f079677467366d9584f40df16fca3ce216
CurosMJ <curosmj@gmail.com>
2015-01-08 12:55:44 +0530

    added copyright and SNS links

--- footer.php
+++ footer.php
@@ -7,32 +7,55 @@
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <ul class="list-inline text-center">
+			<?php
+			$links = array(
+				'fb' => get_theme_mod('fb_link'),
+				'twitter' => get_theme_mod('twitter_link'),
+				'gh' => get_theme_mod('gh_link'),
+                'gplus' => get_theme_mod('gplus_link'),
+				);	
+			 if($links['fb'] != ''): ?>
                         <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
+                            <a href="<?php echo $links['fb']; ?>">
+	                            <span class="fa-stack fa-lg">
+	                    	     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
                                 </span>
                             </a>
                         </li>
+			<?php endif; 
+            if($links['twitter'] != ''): ?>
                         <li>
-                            <a href="#">
+                            <a href="<?php echo $links['twitter']; ?>">
                                 <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                 <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+            <?php endif; 
+            if($links['gplus'] != ''): ?>
+                        <li>
+                            <a href="<?php echo $links['gplus']; ?>">
+                                <span class="fa-stack fa-lg">
+                                 <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+            <?php endif; 
+            if($links['gh'] != ''): ?>
+                        <li>
+                            <a href="<?php echo $links['gh']; ?>">
+                                <span class="fa-stack fa-lg">
+                                 <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-github fa-stack-1x fa-inverse"></i>
                                 </span>
                             </a>
                         </li>
+            <?php endif; ?>
                     </ul>
-                    <p class="copyright text-muted">Copyright &copy; Your Website 2014</p>
+                    <p class="copyright text-muted">Copyright &copy; <?php bloginfo('title');  ?></p>
                 </div>
             </div>
         </div>
--- functions.php
+++ functions.php
@@ -14,19 +14,47 @@ register_nav_menu('primary', 'Header Menu');
 
 function cleanblog_customize_register( $wpCustomize )
 {
+	//Header Color
 	$wpCustomize->add_setting('header_color', array(
 		'default' => '#333'
 		));
 	$wpCustomize->add_control( new WP_Customize_Color_Control( 
-	$wpCustomize, 
-	'header_color', 
-	array(
-		'label'      => __( 'Header Color', 'cleanblog' ),
-		'section' => 'header_image',
-		'settings'   => 'header_color',
-		'priority' => 0,
-	)
-));
+		$wpCustomize, 
+		'header_color', 
+		array(
+			'label'      => 'Header Color',
+			'section' => 'header_image',
+			'settings'   => 'header_color',
+			'priority' => 0,
+		)
+	));
+
+	//Social Media Links
+	$wpCustomize->add_section('sns_links',array(
+		'title' => 'Social Links'
+		));
+	
+	$sns = array(
+		'fb_link' => 'Facebook Link',
+		'twitter_link' => 'Twitter Link',
+		'gh_link' => 'GitHub Link',
+		'gplus_link' => 'Google+ Link');
+
+	foreach ( $sns as $id => $label)
+	{
+		$wpCustomize->add_setting($id, array(
+		'default' => ''
+		));
+		$wpCustomize->add_control( new WP_Customize_Control( 
+		$wpCustomize, 
+		$id, 
+		array(
+			'label'      => $label,
+			'section' => 'sns_links',
+			'settings'   => $id,
+		)
+		));
+	}
 }
 
 add_action( 'customize_register', 'cleanblog_customize_register' );
